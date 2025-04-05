912407ed5946ec7eefe33f49aa4ae5258e7d8378
Vamsi Karanam <you@example.com>
2025-04-04 20:30:12 -0400

    create logo components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Logos from './Components/logos.components'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Logos />
+      <Logos />
       <h1>Vamsi Karanam</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
